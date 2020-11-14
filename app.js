7a66c56aa668313d20d84e29270f235a41166be5
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-11-14 14:30:28 -0500

    added files/changes for heroku deployment

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ var helmet = require('helmet');
 var app = express();
 
 //Set up mongoose connection
+var dev_db_url = 'mongodb+srv://admin:adminpassword@cluster0.pzjrw.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://admin:adminpassword@cluster0.pzjrw.mongodb.net/local_library?retryWrites=true&w=majority';
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
